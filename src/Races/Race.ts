0de4e6ae8ad3c87bdaba3e160f6d9dc0b973f58e
kelsonbatista <kelsonbatista@gmail.com>
2022-06-27 15:59:39 -0300

    feat(races): added class Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,19 @@
+abstract class Race {
+  abstract get maxLifePoints(): number;
+
+  constructor(private _name: string, private _dexterity: number) { }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
+
+export default Race;
