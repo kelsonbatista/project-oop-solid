6ac700d41df24db8c3181352620a443290bfc7d1
kelsonbatista <kelsonbatista@gmail.com>
2022-06-28 00:39:32 -0300

    feat(monster): added Monster class

---
+++ src/Monster.ts
@@ -0,0 +1,31 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  public attack(enemy: SimpleFighter | Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  public receiveDamage(attackPoints: number): number {
+    const def = this.lifePoints;
+    const damage = def - attackPoints;
+    if (damage > 0) this._lifePoints -= 1;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+}
