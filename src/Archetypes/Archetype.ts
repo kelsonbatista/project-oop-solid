808530a316d25fbd1f7ccf77d8e6ab2185793395
kelsonbatista <kelsonbatista@gmail.com>
2022-06-27 21:15:50 -0300

    feat(archetypes): added Archetype class

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,31 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _special: number;
+  private _cost: number;
+  
+  constructor(private _name: string) {
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get special(): number {
+    return this._special;
+  }
+
+  public get cost(): number {
+    return this._cost;
+  }
+
+  abstract get energyType(): EnergyType;
+  
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
+
+export default Archetype;
