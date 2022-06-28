bd975c297ca4d502e11c70be35734ae6bb882fb4
kelsonbatista <kelsonbatista@gmail.com>
2022-06-27 21:44:03 -0300

    feat(archetypes): added Ranger archetype class

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,25 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Ranger extends Archetype {
+  static _instances = 0;
+  private _energyType: EnergyType;
+
+  constructor(_name: string) {
+    super(_name);
+    this._energyType = 'stamina';
+    Ranger._instances += 1;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  public set energyType(value: EnergyType) {
+    this._energyType = value;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Ranger._instances;
+  }
+}
