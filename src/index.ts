c533118106c5037cbd13ba9bf47dcaa650a79e97
kelsonbatista <kelsonbatista@gmail.com>
2022-06-28 01:29:49 -0300

    feat(index): added index.ts file to play

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { PVP } from './Battle';
+import PVE from './Battle/PVE';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+// 13.1
+const player1 = new Character('Jose');
+const player2 = new Character('Joao');
+const player3 = new Character('Maria');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+// 13.2
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+// 13.3
+const pvp = new PVP(player2, player3);
+
+// 13.4
+const pve = new PVE(player1);
+
+// 13.5
+const runBattles = () => {};
+
+export { player1, player2, player3 };
+export { monster1, monster2 };
+export { pvp };
+export { pve };
+export { runBattles };
