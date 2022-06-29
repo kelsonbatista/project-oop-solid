25485202a2f7aed50034e90a083b2d46a734dee0
kelsonbatista <kelsonbatista@gmail.com>
2022-06-28 23:57:21 -0300

    refactor(fighter): pVE class fully refactored

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,5 +1,16 @@
+import Fighter, { SimpleFighter } from '../Fighter';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
-  
+  constructor(
+    player: Fighter,
+    private monsters: SimpleFighter[] | Fighter[],
+  ) {
+    super(player);
+  }
+
+  public fight(): number {
+    this.monsters.forEach((monster) => monster.attack(this.player));
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
 }
