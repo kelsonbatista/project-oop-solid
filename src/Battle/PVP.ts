7dfde9c77929dc1ea743262abf0cb233bb42ad1f
kelsonbatista <kelsonbatista@gmail.com>
2022-06-29 22:28:58 -0300

    refactor(pvp): minor changes in player variable

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -2,15 +2,15 @@ import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  constructor(private _player1: Fighter, private _player2: Fighter) {
-    super(_player1);
-    this._player2 = _player2;
+  constructor(private player1: Fighter, private player2: Fighter) {
+    super(player1);
+    this.player2 = player2;
   }
 
   public fight(): number {
-    if (this._player1.lifePoints > 0 && this._player2.lifePoints > 0) {
-      this._player1.attack(this._player2);
-      this._player2.attack(this._player1);
+    if (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
     }
     return this.player.lifePoints === -1 ? -1 : 1;
   }
