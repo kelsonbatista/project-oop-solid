6207002a3c8623bf4d932972a2ad2354ceb5bd31
kelsonbatista <kelsonbatista@gmail.com>
2022-06-29 22:29:59 -0300

    fix(pve): added player attack

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -4,13 +4,17 @@ import Battle from './Battle';
 export default class PVE extends Battle {
   constructor(
     player: Fighter,
-    private monsters: SimpleFighter[] | Fighter[],
+    private monsters: SimpleFighter[],
   ) {
     super(player);
+    this.monsters = monsters;
   }
 
   public fight(): number {
-    this.monsters.forEach((monster) => monster.attack(this.player));
+    this.monsters.forEach((monster: SimpleFighter) => {
+      this.player.attack(monster);
+      monster.attack(this.player);
+    });
     return this.player.lifePoints === -1 ? -1 : 1;
   }
 }
